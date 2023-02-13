238ce506a8bba3e8962d0726bdec10db81c10004
Camilo Martinez <cajomapa@gmail.com>
2023-02-12 23:10:19 -0500

    projecto terminado

--- app/src/main/java/com/edu/javeriana/MainActivity.java
+++ app/src/main/java/com/edu/javeriana/MainActivity.java
@@ -2,8 +2,11 @@ package com.edu.javeriana;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,25 +18,42 @@ public class MainActivity extends AppCompatActivity {
     int numeroAleatorio = rand.nextInt(101);
     int cont = 0;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v("NumeroA","El numero es:" + numeroAleatorio);
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
+                boolean fin = false;
+
                 int res = Integer.parseInt(binding.entrada.getText().toString());
-                if (numeroAleatorio != res) {
+
+
+                if(numeroAleatorio == res && cont < 3) {
+                    binding.respuesta.setText("Bien Hecho!");
+                    Toast.makeText(getBaseContext(),"Respuesta Correcta",Toast.LENGTH_SHORT).show();
+                }
+                if (numeroAleatorio != res && cont  <3 ) {
                     cont++;
                     binding.respuesta.setText("Vas: " + cont + " Intentos!");
                     Toast.makeText(getBaseContext(),"Fallaste",Toast.LENGTH_SHORT).show();
 
 
 
-                }else {
-                    //corre
+
+
+                }else if(cont >=3) {
+                    binding.respuesta.setText("Perdiste!");
+                    Toast.makeText(getBaseContext(), "No más intentos!", Toast.LENGTH_SHORT).show();
+
+
                 }
 
             }
@@ -41,4 +61,6 @@ public class MainActivity extends AppCompatActivity {
             });
 
     }
+
+
 }
